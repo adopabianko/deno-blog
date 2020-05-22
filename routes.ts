460e4b1144b5c14b31af27bc8c6026ad6736948a
Ado Pabianko <ado.pabianko@railink.co.id>
2020-05-23 02:51:17 +0700

    Set default url

--- routes.ts
+++ routes.ts
@@ -2,6 +2,9 @@ import { Router } from 'https://deno.land/x/oak/mod.ts';
 import { getArticles, addArticle, deleteArticle } from './controllers/ArticleController.ts';
 
 const router = new Router();
+router.get('/', (context) => {
+    context.response.body = "Simple CRUD using Deno JS";
+});
 router.get('/article', getArticles)
     .get('/article/:id', getArticles)
     .post('/article/add', addArticle)
